17145f99e36d6b6b4c931b8d39e8bbfedb21b7d2
nattyp1 <80240494+nattyp1@users.noreply.github.com>
2021-11-08 11:32:28 -0500

    spellcheck 33

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment"), (req, res) => {
+app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
 					 "Your Javascript skills are stellar.",
@@ -19,9 +19,9 @@ app.get("/api/compliment"), (req, res) => {
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
-};
+});
 
-app.get('/api/fortune'), (req,res) => {
+app.get('/api/fortune', (req,res) => {
   const fortune = ['A friend is a present you give yourself.',
           'A smile is your personal welcome mat.', 'A truly rich life contains love and art in abundance.',
          'All your hard work will soon pay off.', 'It is better to deal with problems before they arise.', 
@@ -30,6 +30,6 @@ let randomIndex2 = Math.floor(Math.random() * fortune.length);
 let randomFortune = fortune[randomIndex2]
  
 res.status(200).send(randomFortune)
-};
+});
 
 app.listen(4000,() => console.log("Server running on 4000"));
