dd52582fbfdd61fa977fa22785bf69aeee1edd34
nattyp1 <80240494+nattyp1@users.noreply.github.com>
2021-11-08 10:32:41 -0500

    -m "First Commit"

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+app.get("/api/compliment"), (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+};
+
+// app.listen(4000,() => console.log("Server running on 4000"));
+
+// app.get('/api/fortune'), (req,res) => {
+//   const fortune = ['A friend is a present you give yourself.',
+//           'A smile is your personal welcome mat.', 'A truly rich life contains love and art in abundance.',
+//          'All your hard work will soon pay off.', 'It is better to deal with problems before they arise.', 
+// ];
+
+// let randomFortune = 
+
+// });
